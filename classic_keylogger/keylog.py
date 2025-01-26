e24972699d1e1c852527d0fa816f5920309ad607
nmcph04 <nmcpherson2004@gmail.com>
2025-01-26 11:48:17 -0600

    Added GUI to classic keylogger and k-means clustering for audio segmentation

--- classic_keylogger/keylog.py
+++ classic_keylogger/keylog.py
@@ -1,23 +1,51 @@
+import time
 from pynput import keyboard
+import threading
+import tkinter as tk
 import os
 
-filename = "keylog.txt"
+if os.path.isfile("keylog.txt"):
+    os.remove("keylog.txt")
 
-# This function will be called whenever a key is pressed
-def on_press(key, filename="keylog.txt"):
-    try:
-        # Log the key pressed
-        with open(filename, "a") as log_file:
-            log_file.write(f'{key.char}\n')
-    except AttributeError:
-        # Handle special keys (like Ctrl, Alt, etc.)
-        with open(filename, "a") as log_file:
-            log_file.write(f'{key}\n')
+# Function to handle the pressed key event
+def on_press(key):
+    global chars
+    print(chars, end='\r')
+    chars += 1
+    with open('keylog.txt', 'a') as f:
+        f.write(f'{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())[:-3]}: {key}\n')
 
-path = os.getcwd() + "\\" + filename
-if os.path.exists(path):
-    os.remove(path)
+# Function to stop recording and close the file
+def end():
+    global running
+    running = False
+    
+    # Stop keylogging and wait for it to finish
+    keylogging_thread.join()
+    
+    # Close the Tkinter window
+    root.destroy()
 
-# Start listening to keyboard events
-with keyboard.Listener(on_press=on_press) as listener:
-    listener.join()
+chars = 0
+
+# Create a thread for keylogging to run in background
+running = True
+def start_keylogging():
+    with keyboard.Listener(on_press=on_press) as listener:
+        while running:
+            time.sleep(0.1)
+        listener.stop()
+
+# Set up the main window and stop button
+root = tk.Tk()
+root.title("Key Logger")
+
+stop_button = tk.Button(root, text="Stop Logging", command=end)
+stop_button.pack(pady=20)
+
+# Start keylogging in a separate thread
+keylogging_thread = threading.Thread(target=start_keylogging, daemon=True)
+keylogging_thread.start()
+
+# Run the tkinter main loop
+root.mainloop()
