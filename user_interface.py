fc5d4a599e995f160341ec82a4f49be07c13a8a8
nmcph <nmcpherson2004@gmail.com>
2025-04-27 13:11:57 -0500

    Added settings menu to user interface. Added extra settings for training.

--- user_interface.py
+++ user_interface.py
@@ -30,16 +30,31 @@ def input_validation(min_val, max_val):
     
     return user_input
 
+def reset_settings():
+    if not os.path.exists("default_settings.json"):
+        raise Exception("default_settings.json is missing!")
+    
+    shutil.copyfile('default_settings.json', 'settings.json')
+
 def read_settings():
     if not os.path.exists("settings.json"):
-        if not os.path.exists("default_settings.json"):
-            raise Exception("default_settings.json is missing!")
-        
-        else:
-            shutil.copyfile('default_settings.json', 'settings.json')
+        reset_settings()
     
     with open('settings.json') as f:
         return json.load(f)
+
+def write_settings(settings: dict):
+    with open('settings.json', 'w', encoding='utf-8') as f:
+        json.dump(settings, f, ensure_ascii=False, indent=4)
+
+def str_to_bool(text: str) -> bool:
+    if text.lower() == "true":
+        return True
+    elif text.lower() == "false":
+        return False
+    else:
+        print("boolean setting is not 'true' or 'false'. Defaulting to True.")
+        return True
 
 def ui_train(settings: dict):
     clear_screen()
@@ -48,13 +63,9 @@ def ui_train(settings: dict):
     # Confirm settings, data paths, etc. Then run the classifier training
     print(f"{"Confirm the following settings:" :^{UI_WIDTH}}")
     
-    model_dir = settings['modelPath']
-    print(f"{f"Model path: {model_dir}" :^{UI_WIDTH}}")
-    
-    print(f"{f"Confirm directory deletion: {settings['confirmDirectoryDeletion']}" :^{UI_WIDTH}}")
-    
-    data_dir = settings['trainingDataPath']
-    print(f"{f"Training data path: {data_dir}" :^{UI_WIDTH}}")
+    training_settings = ['modelPath', 'confirmDirectoryDeletion', 'trainingDataPath', 'trainingForcePreprocessing', 'trainingSaveModel']
+    for setting in training_settings:
+        print(' '*9, f"{setting}: {settings[setting]}")
     
     user_confirm = input("Confirm these settings [y/N] ")
     
@@ -63,9 +74,11 @@ def ui_train(settings: dict):
         time.sleep(1)
         return
     
-    print("Training classification model...")
+    settings['trainingSaveModel'] = str_to_bool(settings['trainingSaveModel'])
     
-    train_model(data_dir=data_dir, return_model=False, save_dir=model_dir)
+    print("Training classification model...")
+
+    train_model(data_dir=settings['trainingDataPath'], return_model=False, save_dir=settings['modelPath'], save_model=settings['trainingSaveModel'])
     
     input("Model training complete! Press enter to continue: ")
     return
@@ -87,13 +100,12 @@ def ui_analysis(settings: dict):
         user_input = input_validation(1, 2)
 
         if user_input == 1:
-            # Confirm paths of models and audio file. Then run the models on the file
+            # Confirm paths of models and audio file. Then run the model on the file
             print(f"{"Confirm the following settings:" :^{UI_WIDTH}}")
 
             model_path = settings['modelPath']
             print(f"{f"Model path: {model_path}" :^{UI_WIDTH}}")
 
-            # Maybe make this its own directory
             data_path = settings['analysisDataPath']
             print(f"{f"Data path: {data_path}" :^{UI_WIDTH}}")
 
@@ -113,8 +125,53 @@ def ui_analysis(settings: dict):
             return
 
 def ui_settings(settings: dict):
-    print("WIP")
-    input(f"For now, directly modify settings.json. ")
+    while True:
+        clear_screen()
+        print(f"{"Heimdall Settings" :^{UI_WIDTH}}")
+        print('-'*UI_WIDTH)
+        print(f"{"Select a setting to change:" :^{UI_WIDTH}}")
+
+        options = list(settings.keys())
+        num_options = len(options)
+        for i in range(num_options):
+            print(" "*10, f"{i+1}) {options[i]}: '{settings[options[i]]}'")
+        
+        print(" "*10, f"{num_options+1}) Reset all settings")
+        print(" "*10, f"{num_options+2}) Return to main menu")
+
+        selected = input_validation(1, num_options + 2) - 1
+
+        if selected == num_options:
+            print("All settings will be reset to the defaults in default_settings.json.")
+            confirm = input("Confirm? [y/N] ")
+            if confirm.lower() == 'y':
+                print("Resetting all settings...")
+                reset_settings()
+                settings = read_settings()
+                print("Settings successfully reset!")
+            else:
+                print("Reset not confirmed.")
+
+        elif selected == num_options+1:
+            print("Returning to main menu...")
+            time.sleep(1)
+            break
+
+        else:
+            print(f"Changing {options[selected]} (currently '{settings[options[selected]]}')")
+            new_setting = input("Input the new setting: ")
+
+            print(f"{options[selected]} will be set to {new_setting} (was {settings[options[selected]]})")
+
+            confirm = input("Confirm? [y/N] ")
+            if confirm.lower() == 'y':
+                settings[options[selected]] = new_setting
+                write_settings(settings)
+                print("Setting updated successfully.")
+            else:
+                print("Setting change not confirmed.")
+        
+        time.sleep(1)
 
 def ui():
     settings_json = read_settings()
